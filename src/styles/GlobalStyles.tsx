46b1090a2eb6a1be366a285771de7d38888b95c9
Nastya Koshel <nastyakoshel68@gmail.com>
2025-06-24 23:28:56 +0300

    font

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -5,5 +5,8 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        
+        font-family: 'Roboto', sans-serif;
     }
+    
 `
