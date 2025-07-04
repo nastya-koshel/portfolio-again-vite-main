76aa9608261475b9676b5fe34a75473a17540a8c
Nastya Koshel <nastyakoshel68@gmail.com>
2025-07-04 20:41:13 +0300

    getInTouch decoration (remake)

--- src/layout/sections/get-in-touch/GetInTouch.tsx
+++ src/layout/sections/get-in-touch/GetInTouch.tsx
@@ -2,24 +2,33 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 // import {Button} from "../../../components/button/Button.tsx";
 import {theme} from "../../../styles/Theme.tsx";
+import {Container} from "../../../components/Container.tsx";
 
 export const GetInTouch = () => {
     return (
         <StyledGetInTouch>
-            <FlexWrapper direction="column">
-                <MainText>Have any project in mind?</MainText>
-                <SecondaryText>Let’s work together and I will help you by all my best.</SecondaryText>
-            </FlexWrapper>
-            <Triangle width="172" height="176" viewBox="0 0 172 176" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path opacity="0.05" d="M1.03782 22.6024C-2.94098 9.18534 10.09 -3.02371 23.2197 1.81961L160.697 52.5326C172.56 56.9086 175.664 72.2427 166.437 80.8878L70.6203 170.661C61.3932 179.306 46.2936 175.211 42.6987 163.088L1.03782 22.6024Z" fill="white" />
-            </Triangle>
-            <NewButton>Let's talk</NewButton>
-            {/*<Button text="Let's talk"/>*/}
+            <Container>
+                <Tablet>
+                    <FlexWrapper direction="column">
+                        <MainText>Have any project in mind?</MainText>
+                        <SecondaryText>Let’s work together and I will help you by all my best.</SecondaryText>
+                    </FlexWrapper>
+                    <Triangle width="172" height="176" viewBox="0 0 172 176" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path opacity="0.05" d="M1.03782 22.6024C-2.94098 9.18534 10.09 -3.02371 23.2197 1.81961L160.697 52.5326C172.56 56.9086 175.664 72.2427 166.437 80.8878L70.6203 170.661C61.3932 179.306 46.2936 175.211 42.6987 163.088L1.03782 22.6024Z" fill="white" />
+                    </Triangle>
+                    <NewButton>Let's talk</NewButton>
+                    {/*<Button text="Let's talk"/>*/}
+                </Tablet>
+            </Container>
+
         </StyledGetInTouch>
     )
 }
 
 const StyledGetInTouch = styled.section`
+  
+`
+const Tablet = styled.div`
     background-color: #0FBF61;
     border: 1px solid #000;
     border-radius: 15px;
@@ -27,17 +36,18 @@ const StyledGetInTouch = styled.section`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    
+
     padding: 45px;
-    
-    //хардкод
-    height: 250px;
-    width: 1116px;
+
+
+    min-height: 250px;
+    width: 100%;
 
     margin: auto;
     position: relative;
     top: 50px
 `
+
 const MainText = styled.h3`
     font-weight: 500;
     font-size: 45px;
